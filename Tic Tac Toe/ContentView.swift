638e2ca331c5c5e714d52348b8debe9f17227f7a
Fabiola Rocha Marquez <frochamarquez@bsd220.org>
2023-11-28 18:02:15 -0600

    Initial Commit

---
+++ Tic Tac Toe/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Tic Tac Toe
+//
+//  Created by Fabiola Rocha Marquez on 11/28/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
